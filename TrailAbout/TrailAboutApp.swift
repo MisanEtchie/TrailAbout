e673f033b32d65ca81d815dfbf771163e5a10ec2
Misan Etchie <56762634+MisanEtchie@users.noreply.github.com>
2024-08-25 08:50:18 -0700

    Initial Commit

---
+++ TrailAbout/TrailAboutApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailAboutApp.swift
+//  TrailAbout
+//
+//  Created by Misan on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TrailAboutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
