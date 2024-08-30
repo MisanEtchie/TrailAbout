18e5a5ada5001987b7d57444f67ae9c2f256b29d
Misan Etchie <56762634+MisanEtchie@users.noreply.github.com>
2024-08-29 22:02:20 -0700

    finished with most location implementation

---
+++ TrailAbout/Views/LocationMapAnnotationView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationMapAnnotationView.swift
+//  TrailAbout
+//
+//  Created by Misan on 8/28/24.
+//
+
+import SwiftUI
+
+struct LocationMapAnnotationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationMapAnnotationView()
+}
---
+++ TrailAbout/Views/LocationsPreviewView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsPreviewView.swift
+//  TrailAbout
+//
+//  Created by Misan on 8/28/24.
+//
+
+import SwiftUI
+
+struct LocationsPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsPreviewView()
+}
