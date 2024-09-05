694f2add1217945104567129cc773106b35de052
Misan Etchie <56762634+MisanEtchie@users.noreply.github.com>
2024-09-04 22:56:35 -0700

    started work on crfeate post

---
+++ TrailAbout/Models/Post.swift
@@ -0,0 +1,18 @@
+//
+//  Post.swift
+//  TrailAbout
+//
+//  Created by Misan on 9/4/24.
+//
+
+import SwiftUI
+
+struct Post: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Post()
+}
---
+++ TrailAbout/Views/Posts/CreateNewPost.swift
@@ -0,0 +1,18 @@
+//
+//  CreateNewPost.swift
+//  TrailAbout
+//
+//  Created by Misan on 9/4/24.
+//
+
+import SwiftUI
+
+struct CreateNewPost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateNewPost()
+}
